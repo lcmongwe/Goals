2a1085cab7242f0fa7060cc81beea3b5fca37191
lcmongwe <lcmongwe@gmail.com>
2022-04-06 23:10:14 +0300

    goaldetail component creted

---
+++ src/app/goal-detail/goal-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+  styleUrls: ['./goal-detail.component.css'],
+})
+export class GoalDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
