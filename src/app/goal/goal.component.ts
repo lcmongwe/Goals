9de659a98d825c81945202c6ca9e24e7332377cb
lcmongwe <lcmongwe@gmail.com>
2022-04-07 10:39:42 +0300

    date pipe added

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,12 +8,27 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
-    new Goal(1, 'watch finding nemo', 'it is interesting'),
-    new Goal(2, 'buy cookies', 'i love cookies'),
-    new Goal(3, 'get new phone case', 'diana broke her case'),
-    new Goal(4, 'get dog food', 'food is over'),
-    new Goal(5, 'solve math homework', 'math is good'),
-    new Goal(6, 'plot my world domination plan', 'oooh nice'),
+    new Goal(
+      1,
+      'watch finding nemo',
+      'it is interesting',
+      new Date(2020, 3, 14)
+    ),
+    new Goal(2, 'buy cookies', 'i love cookies', new Date(2020, 3, 14)),
+    new Goal(
+      3,
+      'get new phone case',
+      'diana broke her case',
+      new Date(2020, 3, 14)
+    ),
+    new Goal(4, 'get dog food', 'food is over', new Date(2020, 3, 14)),
+    new Goal(5, 'solve math homework', 'math is good', new Date(2020, 3, 14)),
+    new Goal(
+      6,
+      'plot my world domination plan',
+      'oooh nice',
+      new Date(2020, 3, 14)
+    ),
   ];
   toggleDetails(index: number) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
