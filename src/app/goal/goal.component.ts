3398c6b5f9388451650a700edce9fcc0643efd14
lcmongwe <lcmongwe@gmail.com>
2022-04-11 10:18:34 +0300

    changes

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -39,6 +39,24 @@ export class GoalComponent implements OnInit {
     }
   }
 
+  deleteGoal(isComplete: boolean, index: number) {
+    if (isComplete) {
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.goals[index].name}?`
+      );
+
+      if (toDelete) {
+        this.goals.splice(index, 1);
+      }
+    }
+  }
+  addNewGoal(goal: any) {
+    let goalLength = this.goals.length;
+    goal.id = goalLength + 1;
+    goal.completeDate = new Date(goal.completeDate);
+    this.goals.push(goal);
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
